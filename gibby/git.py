09750a715f785b18da9902f2cb44c3fa76a25f7c
Bip901 <74911767+Bip901@users.noreply.github.com>
2024-03-07 23:19:46 +0200

    Initial commit

---
+++ gibby/git.py
@@ -0,0 +1,45 @@
+import os
+import subprocess
+from pathlib import Path
+from typing import Any, ClassVar, Optional, Union
+
+GIT_DIR_ENVIRONMENT_VAR = "GIT_DIR"
+GIT_DIR_DEFAULT = ".git"
+GIT_EXECUTABLE_ENVIRONMENT_VAR = "GIT_EXECUTABLE"
+GIT_EXECUTABLE_DEFAULT = "git"
+
+
+class Singleton(type):
+    _instance: ClassVar[Optional[Any]] = None
+
+    def __call__(cls, *args, **kwargs) -> Any:
+        if cls._instance is None:
+            cls._instance = super().__call__(*args, **kwargs)
+        return cls._instance
+
+
+class Git(metaclass=Singleton):
+    _instance: ClassVar[Optional["Git"]] = None
+
+    def __init__(self) -> None:
+        self._git_executable: Optional[str] = None
+        self.git_directory_name = os.environ.get(GIT_DIR_ENVIRONMENT_VAR, GIT_DIR_DEFAULT)
+
+    @property
+    def git_executable(self) -> str:
+        if self._git_executable is not None:
+            return self._git_executable
+        self._git_executable = os.environ.get(GIT_EXECUTABLE_ENVIRONMENT_VAR, GIT_EXECUTABLE_DEFAULT)
+        try:
+            subprocess.run(
+                [self._git_executable, "--version"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, check=True
+            )
+        except Exception as ex:
+            raise ValueError(
+                f'Failed running git with "{self._git_executable}". Check that git is installed on your PATH, or set the {GIT_DIR_ENVIRONMENT_VAR} environment variable manually.'
+            ) from ex
+        return self._git_executable
+
+    def run(self, cwd: Union[Path, str], *args: str) -> str:
+        process = subprocess.run([self.git_executable, *args], stdout=subprocess.PIPE, text=True, cwd=cwd, check=True)
+        return process.stdout
---
+++ gibby/remote_url.py
@@ -0,0 +1,95 @@
+import abc
+import logging
+import os
+import platform
+import urllib.parse
+from pathlib import Path
+from typing import Union
+
+from .git import Git
+
+logger = logging.getLogger()
+
+
+class RemoteUrl:
+    def __init__(self, raw_url: str) -> None:
+        self.raw_url = raw_url
+        self._parse_result = urllib.parse.urlparse(raw_url)
+
+    def join_path(self, relative_path: Union[str, Path]) -> "RemoteUrl":
+        new_url = urllib.parse.urljoin(self.raw_url, str(relative_path))
+        return type(self)(new_url)
+
+    def __str__(self) -> str:
+        return self.raw_url
+
+    @abc.abstractmethod
+    def init_git_bare_if_needed(self, permissions: int = 0o777) -> None:
+        """
+        Creates this directory and parent directories as necessary.
+
+        :param permissions: The filesystem permissions to apply to all created directories.
+        """
+        pass
+
+
+class FileRemoteUrl(RemoteUrl):
+    def __init__(self, raw_url: str) -> None:
+        super().__init__(raw_url)
+        if self._parse_result.netloc:
+            raise ValueError(
+                "File URLs with a remote location are not supported. Did you mean file:/// with 3 slashes?"
+            )
+        self._local_path = Path(self._url_path_to_local_path(self._parse_result.path))
+
+    @classmethod
+    def _url_path_to_local_path(cls, quoted_path: str) -> str:
+        local_path = urllib.parse.unquote(quoted_path)
+        if (
+            platform.system().casefold() == "windows"
+            and len(local_path) >= 3
+            and local_path[0] == "/"
+            and local_path[2] == ":"
+        ):
+            return local_path[1:]
+        return local_path
+
+    def __str__(self) -> str:
+        return str(self._local_path)
+
+    def init_git_bare_if_needed(self, permissions: int = 0o777) -> None:
+        missing_directories = []
+        directory = self._local_path
+        while not directory.exists():
+            missing_directories.append(directory)
+            next_directory = directory.parent
+            if directory == next_directory:
+                break
+            directory = next_directory
+        for directory in reversed(missing_directories):
+            directory.mkdir(mode=permissions)
+        if next(directory.iterdir(), None) is None:
+            logger.info(f"Initializing new git repo at {directory}")
+            Git().run(self._local_path, "init", "--bare")
+
+
+KNOWN_SCHEMES: dict[str, type[RemoteUrl]] = {"file": FileRemoteUrl}
+
+
+def parse(url_string: str) -> RemoteUrl:
+    try:
+        scheme_end_index = url_string.index(
+            "://"
+        )  # The // is technically optional according to the URI spec, but we need to support Windows paths that contain ':' (C:/Foo)
+        scheme = url_string[:scheme_end_index]
+    except ValueError:  # No explicit scheme
+        canon_local_path = str(Path(url_string).absolute()).replace(os.sep, "/")
+        url_string = "file:///" + urllib.parse.quote(canon_local_path)
+        scheme = "file"
+    if not url_string.endswith("/"):
+        url_string += "/"  # Mark as a directory for proper urljoin
+
+    if scheme in KNOWN_SCHEMES:
+        return KNOWN_SCHEMES[scheme](url_string)
+    else:
+        raise ValueError(f"Unsupported scheme '{scheme}'.")
